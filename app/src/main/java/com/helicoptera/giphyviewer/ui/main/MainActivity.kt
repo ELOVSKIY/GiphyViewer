ae08d53eb1a46ba53be47ba59887be911de73e62
Stas Elovsky <stas.elovsky@gmail.com>
2019-09-30 10:47:12 +0300

    Final version

--- app/src/main/java/com/helicoptera/giphyviewer/ui/main/MainActivity.kt
+++ app/src/main/java/com/helicoptera/giphyviewer/ui/main/MainActivity.kt
@@ -6,21 +6,17 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.SearchView
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.helicoptera.giphyviewer.Utils.hasNetConnection
 import com.helicoptera.giphyviewer.R
-import com.helicoptera.giphyviewer.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewmodel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        viewmodel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         setSupportActionBar(toolbar)
     }
 
